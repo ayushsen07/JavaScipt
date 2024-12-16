36dd889267eb3708741f8279a216ed82d1e630c4
Ayush sen <senayush920@gmail.com>
2024-12-16 19:01:20 +0530

    Classes and object

---
+++ Classes_&_OOPs/abstract.js
@@ -0,0 +1,26 @@
+
+// Abstract :- is used to hide some secret info from the developers ot users  for making abstract variable use _ this before variable it shows a variable is secret
+
+
+class Student{
+    constructor(name){
+        this.name = name;
+        this._accoutNo='435345';
+    }
+     get getSecret(){
+        return this._accoutNo
+     }
+     set setSecret(newSecret){
+         this._accoutNo=newSecret
+       }
+
+}
+
+const amit = new Student('amit')
+console.log(amit);
+console.log(amit.getSecret);
+amit.setSecret= '87878'
+console.log(amit);
+
+
+
--- Classes_&_OOPs/class.js
+++ Classes_&_OOPs/class.js
@@ -1,3 +1,44 @@
 class Person{
-    
+    constructor(name,place){
+        this.name=name;
+        this.place=place;
+    }
+
+    sayhi(){
+        console.log(`Hi ${this.name}`);
+        
+    }
 }
+
+const hitesh = new Person('hitesh' , 'jaipur')
+const Vishwa = new Person('Vishwa' , 'Benguluru')
+console.log(new Person('ayush' , 'sehore'));
+
+console.log(hitesh);
+console.log(Vishwa);
+
+
+
+
+// Class Expression
+// Why calss expression :- Class Expression is here for storing calss in sife a variable and passed to any other function or variable
+
+// class are not hoisted  
+
+
+
+const Student  = class{
+    constructor(name){
+        this.name = name;
+    }
+
+    sayHi(){
+        console.log(`hello ${this.name}`);
+        
+    }
+}
+
+console.log(new Student('ayush'));
+const ayush = new Student('ayush')
+console.log(ayush);
+
